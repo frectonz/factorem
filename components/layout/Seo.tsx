a88bf81243eb7c7608497a30b95d09be0a39ef87
fraol0912 <fraol0912@gmail.com>
2021-09-18 01:44:24 +0300

    add descriptio field

--- components/layout/Seo.tsx
+++ components/layout/Seo.tsx
@@ -8,6 +8,10 @@ export default function Seo({ title }: SeoProps) {
   return (
     <Head>
       <title>{title && `${title} - `} Factorem</title>
+      <meta
+        name="description"
+        content="This fraol lemecha's personal blog about tech called factorem."
+      />
     </Head>
   );
 }
