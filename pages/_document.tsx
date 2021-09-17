a4ef7cf18a1de021c4a263ebc674bfc5557828fe
fraol0912 <fraol0912@gmail.com>
2021-09-18 01:32:33 +0300

    change default theme to dark

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+// ChakraUI
+import theme from "../components/theme";
+import { ColorModeScript } from "@chakra-ui/react";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
